700f30b6111db11c3781bfcf4ecd51ef9c0e80a7
Demo-Repos <khushrajrai@gmail.com>
2026-03-25 00:25:09 +0530

    Added Pascal Triangle Pattern

--- java/java-questions/PascalTriangle.java
+++ java/java-questions/PascalTriangle.java
@@ -3,3 +3,28 @@
 //     1  2  1
 //   1  3  3  1
 // 1  4  6  4  1
+
+import java.util.Scanner;
+
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            //space
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            //Main Pattern
+            int num = 1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(num + " ");
+                num = num * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
