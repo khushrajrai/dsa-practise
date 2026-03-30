ecf067e67ba7bd0f7637a951abd5724516447a3d
Demo-Repos <khushrajrai@gmail.com>
2026-03-30 12:46:38 +0530

    Added Linear Search Problem

--- leetcode/744th Find Smallest Letter Greater Than Target.java
+++ leetcode/744th Find Smallest Letter Greater Than Target.java
@@ -30,3 +30,25 @@
 // letters contains at least two different characters.
 // target is a lowercase English letter.
 
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char res = binarySearchCeil(letters, target);
+        return res;
+    }
+    static char binarySearchCeil(char arr[], char target){
+        char ans = '$';
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]<=target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+                ans = arr[mid];
+            }
+        }
+        return (ans=='$')?arr[0]:ans;
+    }
+}
