a52eefa1d26ce088bad4717606da27d3a59ec954
Demo-Repos <khushrajrai@gmail.com>
2026-03-30 19:24:57 +0530

    Added Guess Number Question

--- leetcode/1295th Find Numbers with Even Number of Digits.java
+++ leetcode/1295th Find Numbers with Even Number of Digits.java
@@ -1,10 +1,7 @@
 // 1295. Find Numbers with Even Number of Digits
 // Given an array nums of integers, return how many of them contain an even number of digits.
 
- 
-
 // Example 1:
-
 // Input: nums = [12,345,2,6,7896]
 // Output: 2
 // Explanation: 
@@ -15,14 +12,23 @@
 // 7896 contains 4 digits (even number of digits). 
 // Therefore only 12 and 7896 contain an even number of digits.
 // Example 2:
-
 // Input: nums = [555,901,482,1771]
 // Output: 1 
 // Explanation: 
 // Only 1771 contains an even number of digits.
- 
-
 // Constraints:
-
 // 1 <= nums.length <= 500
 // 1 <= nums[i] <= 105
+class Solution {
+
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            String s = Integer.toString(nums[i]);
+            if (s.length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
