ea52ebaadf086528cc4085aa3422e1dccbbef3e1
Demo-Repos <khushrajrai@gmail.com>
2026-04-02 20:56:56 +0530

    Added Binary Search Question

--- leetcode/540th Single Element in a Sorted Array.java
+++ leetcode/540th Single Element in a Sorted Array.java
@@ -1,24 +1,45 @@
-540. Single Element in a Sorted Array
-You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+// 540. Single Element in a Sorted Array
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
 
-Return the single element that appears only once.
+// Return the single element that appears only once.
 
-Your solution must run in O(log n) time and O(1) space.
+// Your solution must run in O(log n) time and O(1) space.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [1,1,2,3,3,4,4,8,8]
-Output: 2
-Example 2:
+// Input: nums = [1,1,2,3,3,4,4,8,8]
+// Output: 2
+// Example 2:
 
-Input: nums = [3,3,7,7,10,11,11]
-Output: 10
+// Input: nums = [3,3,7,7,10,11,11]
+// Output: 10
  
 
-Constraints:
+// Constraints:
 
-1 <= nums.length <= 105
-0 <= nums[i] <= 105
+// 1 <= nums.length <= 105
+// 0 <= nums[i] <= 105
 
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if (nums[mid] == nums[mid+1]){
+                mid = mid-1;
+            }
+            int leftPartLen = (mid-start)+1;
+            if (leftPartLen % 2 ==1){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return nums[start];
+    }
+}
