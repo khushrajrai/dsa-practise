2c36e902d948c406f1cc75808ad3136996c64210
Demo-Repos <khushrajrai@gmail.com>
2026-04-03 14:37:40 +0530

    Added Koko Eating Bananas Problem

--- gfg/Aggressive Cows.java
+++ gfg/Aggressive Cows.java
@@ -2,7 +2,6 @@
 // You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
 
 // Examples:
-
 // Input: stalls[] = [1, 2, 4, 8, 9], k = 3
 // Output: 3
 // Explanation: The first cow can be placed at stalls[0], 
@@ -23,4 +22,44 @@
 // 2 ≤ stalls.size() ≤ 106
 // 0 ≤ stalls[i] ≤ 108
 // 2 ≤ k ≤ stalls.size()
+class Solution {
 
+    public int aggressiveCows(int[] stalls, int k) {
+        // code here
+        Arrays.sort(stalls);
+        int start = Integer.MAX_VALUE;
+        int end = 0;
+        int n = stalls.length;
+        for (int i = 0; i < n - 1; i++) {
+            start = Math.min(start, stalls[i + 1] - stalls[i]);
+        }
+        end = stalls[n - 1] - stalls[0];
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isAllocationPossible(stalls, mid, k)) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isAllocationPossible(int stalls[], int minDist, int cows) {
+        int n = stalls.length;
+        int count = 1;
+        int lp = stalls[0];
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - lp >= minDist) {
+                count++;
+                lp = stalls[i];
+            }
+            if (count >= cows) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
