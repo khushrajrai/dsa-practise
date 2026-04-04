de64125db3a2021cdeb1715b7f5368750e8d6c2d
Demo-Repos <khushrajrai@gmail.com>
2026-04-04 20:24:23 +0530

    Added Arranging Coins Question

--- leetcode/4th Median of Two Sorted Arrays.java
+++ leetcode/4th Median of Two Sorted Arrays.java
@@ -1,29 +1,64 @@
 // 4. Median of Two Sorted Arrays
 
 // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-
 // The overall run time complexity should be O(log (m+n)).
-
- 
-
 // Example 1:
-
 // Input: nums1 = [1,3], nums2 = [2]
 // Output: 2.00000
 // Explanation: merged array = [1,2,3] and median is 2.
 // Example 2:
-
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
- 
-
 // Constraints:
-
 // nums1.length == m
 // nums2.length == n
 // 0 <= m <= 1000
 // 0 <= n <= 1000
 // 1 <= m + n <= 2000
 // -106 <= nums1[i], nums2[i] <= 106
+class Solution {
 
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // make num1 as min length array.
+        // call same function, and change the params
+        // if nums1 was greater and nums2 was smaller
+        // so because we swapped these two, now nums1 is smaller and
+        // nums2 is greater.
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        // nums1 is smaller and num2 is greater.
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int N = n1 + n2;
+        int start = 0;
+        int end = n1;
+        while (start <= end) {
+            // mid -> cut1
+            int cut1 = start + (end - start) / 2;
+            // (n1 + n2)/2 - length of cut1
+            int cut2 = N / 2 - cut1;
+            //l1,l2,r1,r2
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // check if cut is valid
+            if (l1 <= r2 && l2 <= r1) {
+                //if N is odd , only one median
+                if (N % 2 != 0) {
+                    return (double) Math.min(r1, r2);
+                } else {
+                    return ((Math.max(l1, l2) + Math.min(r1, r2)) / 2.0);
+                }
+            } else if (l1 > r2) {
+                end = cut1 - 1;
+            } else {
+                start = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+}
