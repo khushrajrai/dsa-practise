11bffa81bcc2d2ae1bb9d05addbfe891955e0a50
Demo-Repos <khushrajrai@gmail.com>
2026-04-07 19:40:07 +0530

    Added Bit Manipulation Solution for LC Ques - Single Element in a Sorted Array

--- leetcode/540th Single Element in a Sorted Array.java
+++ leetcode/540th Single Element in a Sorted Array.java
@@ -2,44 +2,56 @@
 // You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
 
 // Return the single element that appears only once.
-
 // Your solution must run in O(log n) time and O(1) space.
-
- 
-
 // Example 1:
-
 // Input: nums = [1,1,2,3,3,4,4,8,8]
 // Output: 2
 // Example 2:
-
 // Input: nums = [3,3,7,7,10,11,11]
 // Output: 10
- 
-
 // Constraints:
-
 // 1 <= nums.length <= 105
 // 0 <= nums[i] <= 105
-
 class Solution {
+
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
+        //using bit manipulation and binary search
         int start = 0;
-        int end = n-1;
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if (nums[mid] == nums[mid+1]){
-                mid = mid-1;
-            }
-            int leftPartLen = (mid-start)+1;
-            if (leftPartLen % 2 ==1){
-                end = mid;
-            }
-            else{
-                start = mid+1;
+        int end = nums.length - 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            //currenlty in the leftpart
+            if (nums[mid] == nums[mid ^ 1]) {
+                start = mid + 1;
+            } //currently in the rightpart 
+            else {
+                end = mid - 1;
             }
         }
         return nums[start];
     }
 }
+
+//Other Solution 
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         int n = nums.length;
+//         int start = 0;
+//         int end = n-1;
+//         while(start<end){
+//             int mid = start + (end-start)/2;
+//             if (nums[mid] == nums[mid+1]){
+//                 mid = mid-1;
+//             }
+//             int leftPartLen = (mid-start)+1;
+//             if (leftPartLen % 2 ==1){
+//                 end = mid;
+//             }
+//             else{
+//                 start = mid+1;
+//             }
+//         }
+//         return nums[start];
+//     }
+// }
+
