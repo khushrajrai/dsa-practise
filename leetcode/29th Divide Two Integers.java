19a69b77ca1a6e1c4875e33120fcb94dfb4a03e2
Demo-Repos <khushrajrai@gmail.com>
2026-04-08 16:26:42 +0530

    Added Intersection of Two Arrays Leetcode Question'

--- leetcode/29th Divide Two Integers.java
+++ leetcode/29th Divide Two Integers.java
@@ -27,3 +27,25 @@ Constraints:
 -231 <= dividend, divisor <= 231 - 1
 divisor != 0
 
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        boolean isNegative = dividend<0 ^ divisor<0;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int quotient = 0;
+        while(dividend - divisor >=0){
+            int curDivisor = divisor;
+            int curQuotient = 1;
+            while(dividend - (curDivisor<<1)>=0){
+                curDivisor = curDivisor<<1;
+                curQuotient = curQuotient<<1;
+            }
+            quotient += curQuotient;
+            dividend -= curDivisor;
+        }
+        return isNegative? -quotient : quotient;
+    }
+}
