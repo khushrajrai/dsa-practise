1222d9a125367a47fc9faf36c23ce48d6e250cf6
Demo-Repos <khushrajrai@gmail.com>
2026-04-09 11:40:14 +0530

    Added Number of 1 Bits Quesiton

--- leetcode/231st Power of Two.java
+++ leetcode/231st Power of Two.java
@@ -19,12 +19,24 @@
 class Solution {
 
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
+        //using Bit manipulatin
+        if (n > 0 && (n & (n - 1)) == 0) {
+            return true;
+        } else {
             return false;
         }
-        while (n % 2 == 0) {
-            n = n / 2;
-        }
-        return n == 1;
     }
 }
+
+//Brute Force 
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<=0){
+//             return false;
+//         }
+//         while(n%2==0){
+//             n = n/2;
+//         }
+//         return n==1;
+//     }
+// }
