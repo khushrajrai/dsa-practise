6134f3ab48123db0e1c565f689d8906d477750bb
Demo-Repos <khushrajrai@gmail.com>
2026-04-11 15:31:06 +0530

    Added Minimum Number of Days to Make m Bouquets Question

--- leetcode/242nd Valid Anagram.java
+++ leetcode/242nd Valid Anagram.java
@@ -1,25 +1,33 @@
 // 242. Valid Anagram
 
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
- 
-
 // Example 1:
-
 // Input: s = "anagram", t = "nagaram"
-
 // Output: true
-
 // Example 2:
-
 // Input: s = "rat", t = "car"
-
 // Output: false
-
- 
-
 // Constraints:
-
 // 1 <= s.length, t.length <= 5 * 104
 // s and t consist of lowercase English letters.
+class Solution {
 
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] charCounts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charCounts[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            charCounts[t.charAt(i) - 'a']--;
+        }
+        for (int count : charCounts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
