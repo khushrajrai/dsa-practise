d9c71d4323c6e2ce71fced8bb6d2f69330d229fb
Demo-Repos <khushrajrai@gmail.com>
2026-04-13 10:41:33 +0530

    Added Missing Number Question

--- leetcode/263rd Ugly Number.java
+++ leetcode/263rd Ugly Number.java
@@ -1,29 +1,39 @@
-263. Ugly Number
+// 263. Ugly Number
 
-An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+// An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
 
-Given an integer n, return true if n is an ugly number.
+// Given an integer n, return true if n is an ugly number.
 
  
 
-Example 1:
+// Example 1:
 
-Input: n = 6
-Output: true
-Explanation: 6 = 2 × 3
-Example 2:
+// Input: n = 6
+// Output: true
+// Explanation: 6 = 2 × 3
+// Example 2:
 
-Input: n = 1
-Output: true
-Explanation: 1 has no prime factors.
-Example 3:
+// Input: n = 1
+// Output: true
+// Explanation: 1 has no prime factors.
+// Example 3:
 
-Input: n = 14
-Output: false
-Explanation: 14 is not ugly since it includes the prime factor 7.
+// Input: n = 14
+// Output: false
+// Explanation: 14 is not ugly since it includes the prime factor 7.
  
 
-Constraints:
+// Constraints:
 
--231 <= n <= 231 - 1
+// -231 <= n <= 231 - 1
 
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        while(n%2==0) n=n/2;
+        while(n%3==0) n=n/3;
+        while(n%5==0) n=n/5;
+
+        return n==1;
+    }
+}
