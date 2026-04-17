acf974c8500c5aa51663c3758d10302a7864773d
Demo-Repos <khushrajrai@gmail.com>
2026-04-17 15:08:17 +0530

    Added Leetcode Question of Rotate Image

--- gfg/Rotate by 90 degree.java
+++ gfg/Rotate by 90 degree.java
@@ -1,9 +1,7 @@
 // Rotate by 90 degree
 
 // Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
-
 // Examples:
-
 // Input: mat[][] = [[0, 1, 2], 
 //                 [3, 4, 5], 
 //                 [6, 7, 8]] 
@@ -17,4 +15,34 @@
 // Constraints:
 // 1 ≤ n ≤ 102
 // 0 ≤ mat[i][j] ≤ 103
+class Solution {
 
+    public void rotateMatrix(int[][] matrix) {
+        transpose(matrix);
+        int n = matrix.length;
+        //reverse rows - for clockwise and swap rows - for anti-clockwise
+        //here we have to rotate clockwise
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+    }
+
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix, i, j);
+            }
+        }
+    }
+
+    public void swap(int[][] matrix, int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+}
