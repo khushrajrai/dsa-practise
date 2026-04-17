27910fd3eb5e9624118529d85e68e198981552f9
Demo-Repos <khushrajrai@gmail.com>
2026-04-17 13:12:19 +0530

    Added Transpose of Matrix Question

--- leetcode/240th Search a 2D Matrix II.java
+++ leetcode/240th Search a 2D Matrix II.java
@@ -1,25 +1,15 @@
 // 240. Search a 2D Matrix II
 
 // Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
-
 // Integers in each row are sorted in ascending from left to right.
 // Integers in each column are sorted in ascending from top to bottom.
- 
-
 // Example 1:
-
-
 // Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
 // Output: true
 // Example 2:
-
-
 // Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
 // Output: false
- 
-
 // Constraints:
-
 // m == matrix.length
 // n == matrix[i].length
 // 1 <= n, m <= 300
@@ -27,4 +17,25 @@
 // All the integers in each row are sorted in ascending order.
 // All the integers in each column are sorted in ascending order.
 // -109 <= target <= 109
+class Solution {
 
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int r = 0;
+        int c = cols - 1;
+        while (r < rows && c >= 0) {
+            if (matrix[r][c] == target) {
+                return true;
+            } else if (target < matrix[r][c]) {
+                c = c - 1;
+            } else {
+                r = r + 1;
+            }
+        }
+        return false;
+    }
+}
