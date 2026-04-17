0b50b5db12c7b6086053e6ff9cd461e377323223
Demo-Repos <khushrajrai@gmail.com>
2026-04-17 15:06:51 +0530

    Added GFG Question : Rotate by 90 degree

--- gfg/Transpose of Matrix.java
+++ gfg/Transpose of Matrix.java
@@ -2,9 +2,7 @@
 
 // You are given a square matrix of size n x n. Your task is to find the transpose of the given matrix.
 // The transpose of a matrix is obtained by converting all the rows to columns and all the columns to rows.
-
 // Examples :
-
 // Input: mat[][] = [[1, 1, 1, 1],
 //                 [2, 2, 2, 2],
 //                 [3, 3, 3, 3],
@@ -22,4 +20,33 @@
 // Constraints:
 // 1 ≤ n ≤ 103
 // -109 ≤ mat[i][j] ≤109
+class Solution {
 
+    public ArrayList<ArrayList<Integer>> transpose(int[][] matrix) {
+        // code here
+        int n = matrix.length;
+        // transpose in-place
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix, i, j);
+            }
+        }
+        //now convert it in arraylist
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(matrix[i][j]);
+            }
+            result.add(row);
+        }
+        return result;
+
+    }
+
+    public void swap(int[][] matrix, int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+}
