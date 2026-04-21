f92785ac57b7680216d02bfc75a1dfa5f92c1c8c
Demo-Repos <khushrajrai@gmail.com>
2026-04-21 16:18:46 +0530

    Added Swap Sex of Employees Leetcode SQL Question

--- leetcode/367th Valid Perfect Square.java
+++ leetcode/367th Valid Perfect Square.java
@@ -1,25 +1,46 @@
-367. Valid Perfect Square
+// 367. Valid Perfect Square
 
-Given a positive integer num, return true if num is a perfect square or false otherwise.
+// Given a positive integer num, return true if num is a perfect square or false otherwise.
 
-A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+// A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
 
-You must not use any built-in library function, such as sqrt.
+// You must not use any built-in library function, such as sqrt.
 
  
 
-Example 1:
+// Example 1:
 
-Input: num = 16
-Output: true
-Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
-Example 2:
+// Input: num = 16
+// Output: true
+// Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+// Example 2:
 
-Input: num = 14
-Output: false
-Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+// Input: num = 14
+// Output: false
+// Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
  
 
-Constraints:
+// Constraints:
 
-1 <= num <= 231 - 1
+// 1 <= num <= 231 - 1
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            long sqr = (long)mid*mid;
+            if(sqr==num){
+                return true;
+            }
+            else if(sqr<num){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+}
