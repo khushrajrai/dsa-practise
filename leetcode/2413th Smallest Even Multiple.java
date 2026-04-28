1731963162a7871cb4f3812123e6a183931bea5b
Demo-Repos <khushrajrai@gmail.com>
2026-04-28 08:56:44 +0530

    Added Find Common Elements Between Two Arrays Leetcode

--- leetcode/2413th Smallest Even Multiple.java
+++ leetcode/2413th Smallest Even Multiple.java
@@ -19,3 +19,13 @@
 
 // 1 <= n <= 150
 
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }
+        else{
+            return n*2;
+        }
+    }
+}
