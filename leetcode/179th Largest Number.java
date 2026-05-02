dae250424c4fb30a80cd6649f8aa7bb5d86dc190
Demo-Repos <khushrajrai@gmail.com>
2026-05-02 13:57:25 +0530

    Added Count Integers With Even Digit Sum

--- leetcode/179th Largest Number.java
+++ leetcode/179th Largest Number.java
@@ -1,23 +1,43 @@
 // 179. Largest Number
 
 // Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
-
 // Since the result may be very large, so you need to return a string instead of an integer.
-
- 
-
 // Example 1:
-
 // Input: nums = [10,2]
 // Output: "210"
 // Example 2:
-
 // Input: nums = [3,30,34,5,9]
 // Output: "9534330"
- 
-
 // Constraints:
-
 // 1 <= nums.length <= 100
 // 0 <= nums[i] <= 109
+class Solution {
 
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        String[] stringNum = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            stringNum[i] = String.valueOf(nums[i]);
+        }
+        Comparator<String> comp = new Comparator<String>() {
+            @Override
+            public int compare(String str1, String str2) {
+                String s1 = str1 + str2;
+                String s2 = str2 + str1;
+                return s2.compareTo(s1);
+            }
+        };
+        Arrays.sort(stringNum, comp);
+        if (stringNum[0].charAt(0) == '0') {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String val : stringNum) {
+            sb.append(val);
+        }
+        return sb.toString();
+
+    }
+}
