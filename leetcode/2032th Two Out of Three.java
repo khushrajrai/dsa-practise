1c0a44a1d7d4ff304627e44c87b65f6a3e694f01
Demo-Repos <khushrajrai@gmail.com>
2026-05-03 19:04:16 +0530

    Added Two sum -Pairs with 0 Sum GFG Problem

--- leetcode/2032th Two Out of Three.java
+++ leetcode/2032th Two Out of Three.java
@@ -30,3 +30,37 @@
 // 1 <= nums1.length, nums2.length, nums3.length <= 100
 // 1 <= nums1[i], nums2[j], nums3[k] <= 100
 
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        // make each array valuee unique 
+        // make a frequency map using hashmap adding all arrays occuring
+        // return the elements as new array having frequency 2 or more than 2
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> set3 = new HashSet<>();
+
+        for(int val:nums1) set1.add(val);
+        for(int val:nums2) set2.add(val);
+        for(int val:nums3) set3.add(val);
+
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        for(int element: set1){
+            freqMap.put(element,freqMap.getOrDefault(element,0)+1);
+        }    
+        for(int element: set2){
+            freqMap.put(element,freqMap.getOrDefault(element,0)+1);
+        }    
+        for(int element: set3){
+            freqMap.put(element,freqMap.getOrDefault(element,0)+1);
+        }
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int element : freqMap.keySet()){
+            if(freqMap.get(element)>1){
+                res.add(element);
+            }
+        }
+        
+        return res;
+    }
+}
