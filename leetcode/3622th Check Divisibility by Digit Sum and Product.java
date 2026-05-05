bf3541872f94c41d56cc3a5ae68f058aa64247e8
Demo-Repos <khushrajrai@gmail.com>
2026-05-05 08:51:36 +0530

    Added Bitwise OR of Even Numbers in an Array

--- leetcode/3622th Check Divisibility by Digit Sum and Product.java
+++ leetcode/3622th Check Divisibility by Digit Sum and Product.java
@@ -36,3 +36,21 @@
 
 // 1 <= n <= 106
 
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        int temp=n;
+        while(temp>0){
+            int digit = temp%10;
+            sum+=digit;
+            product*=digit;
+            temp/=10;
+        }
+        if(n%(sum+product)==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
