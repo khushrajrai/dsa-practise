42a8beddf095a43f134aabc4d148211806fbb826
Demo-Repos <khushrajrai@gmail.com>
2026-05-07 10:33:52 +0530

    Added Number of Employees Who Met the Target Leetcode Question

--- leetcode/2154th Keep Multiplying Found Values by Two.java
+++ leetcode/2154th Keep Multiplying Found Values by Two.java
@@ -32,3 +32,20 @@
 
 // 1 <= nums.length <= 1000
 // 1 <= nums[i], original <= 1000
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        boolean found = true;
+        while(found){
+            found = false;
+            for(int num : nums){
+                if(num==original){
+                    original = original*2;
+                    found = true;
+                    break;
+                }
+            }
+        }
+        return original;
+    }
+}
