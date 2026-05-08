1f4ef50f6bb7eac01598553f094d8900f3229253
Demo-Repos <khushrajrai@gmail.com>
2026-05-08 15:58:55 +0530

    Added Maximum Product Subarray Leetcode Question

--- leetcode/53th Maximum Subarray.java
+++ leetcode/53th Maximum Subarray.java
@@ -26,3 +26,16 @@
 // 1 <= nums.length <= 105
 // -104 <= nums[i] <= 104
 
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int currentSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            currentSum+=nums[i];
+            maxSum = Math.max(currentSum,maxSum);
+            if(currentSum<0) currentSum=0;
+        }
+        return maxSum;
+    }
+}
