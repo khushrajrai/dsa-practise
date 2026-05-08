659579937017241335d6eedd9b216163e44cd70c
Demo-Repos <khushrajrai@gmail.com>
2026-05-08 15:29:20 +0530

    Added Maximum Subarray Leetcode Question

--- leetcode/560th Subarray Sum Equals K.java
+++ leetcode/560th Subarray Sum Equals K.java
@@ -22,3 +22,18 @@
 // -1000 <= nums[i] <= 1000
 // -107 <= k <= 107
 
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer,Integer> sumFreqMap = new HashMap<>();
+        sumFreqMap.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            int rem = sum-k;
+            count+=sumFreqMap.getOrDefault(rem,0);
+            sumFreqMap.put(sum,sumFreqMap.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
