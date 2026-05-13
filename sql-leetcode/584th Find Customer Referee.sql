248cb9bd51d6aea226db06bbb63f3547c129fef3
Demo-Repos <khushrajrai@gmail.com>
2026-05-13 11:57:59 +0530

    Added Longest subarray with sum divisible by K

--- sql-leetcode/584th Find Customer Referee.sql
+++ sql-leetcode/584th Find Customer Referee.sql
@@ -10,7 +10,7 @@
 // | referee_id  | int     |
 // +-------------+---------+
 // In SQL, id is the primary key column for this table.
-// Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+// Each row of the table indicates the id of a customer, their name, and the id of the customer who referred them.
  
 
 // Find the names of the customer that are either:
--- sql-leetcode/620th Not Boring Movies.sql
+++ sql-leetcode/620th Not Boring Movies.sql
@@ -45,3 +45,5 @@
 -- Explanation: 
 -- We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 
+# Write your MySQL query statement below
+Select * from Cinema where id%2!=0 and description!="boring" Order by rating DESC;
