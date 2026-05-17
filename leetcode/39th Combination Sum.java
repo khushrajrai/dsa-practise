d5c4587226911489544d3735c61d4a27fa0382f2
Demo-Repos <khushrajrai@gmail.com>
2026-05-17 14:07:50 +0530

    Added Concatenation of Array Leetcode Question

--- leetcode/39th Combination Sum.java
+++ leetcode/39th Combination Sum.java
@@ -1,15 +1,9 @@
 // 39. Combination Sum
 
 // Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
-
 // The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
-
 // The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
-
- 
-
 // Example 1:
-
 // Input: candidates = [2,3,6,7], target = 7
 // Output: [[2,2,3],[7]]
 // Explanation:
@@ -17,19 +11,36 @@
 // 7 is a candidate, and 7 = 7.
 // These are the only two combinations.
 // Example 2:
-
 // Input: candidates = [2,3,5], target = 8
 // Output: [[2,2,2,2],[2,3,3],[3,5]]
 // Example 3:
-
 // Input: candidates = [2], target = 1
 // Output: []
- 
-
 // Constraints:
-
 // 1 <= candidates.length <= 30
 // 2 <= candidates[i] <= 40
 // All elements of candidates are distinct.
 // 1 <= target <= 40
+class Solution {
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        sum(0, target, new ArrayList<Integer>(), result, candidates);
+        return result;
+    }
+
+    public void sum(int startIndex, int remaining, List<Integer> current, List<List<Integer>> result, int candidates[]) {
+        if (remaining == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        } else if (remaining < 0) {
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            int num = candidates[i];
+            current.add(num);
+            sum(i, remaining - num, current, result, candidates);
+            current.remove(current.size() - 1);
+        }
+    }
+}
