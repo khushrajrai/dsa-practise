933dbc6e1a1ebd7280960a6e510326715d559481
Demo-Repos <khushrajrai@gmail.com>
2026-05-18 14:37:16 +0530

    Added 1365. How Many Numbers Are Smaller Than the Current Number Leetcode Question

--- leetcode/1929th Concatenation of Array.java
+++ leetcode/1929th Concatenation of Array.java
@@ -30,3 +30,16 @@
 // 1 <= n <= 1000
 // 1 <= nums[i] <= 1000.
 
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans = new int[n*2];
+        for(int i=0; i<n;i++){
+            ans[i] = nums[i];
+        }
+        for(int i=0;i<n;i++){
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+}
