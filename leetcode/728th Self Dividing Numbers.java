c788a4d515b082d92350b51aca851a1a692dd50f
Demo-Repos <khushrajrai@gmail.com>
2026-05-20 12:52:45 +0530

    Added Self Dividing Numbers Leetcode Question

---
+++ leetcode/728th Self Dividing Numbers.java
@@ -0,0 +1,37 @@
+// 728. Self Dividing Numbers
+
+// A self-dividing number is a number that is divisible by every digit it contains.
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+// A self-dividing number is not allowed to contain the digit zero.
+// Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
+// Example 1:
+// Input: left = 1, right = 22
+// Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+// Example 2:
+// Input: left = 47, right = 85
+// Output: [48,55,66,77]
+// Constraints:
+// 1 <= left <= right <= 104
+class Solution {
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        while (left <= right) {
+            int num = left;
+            boolean isSelfDividingNumber = true;
+            while (num != 0) {
+                int digit = num % 10;
+                num /= 10;
+                if (digit == 0 || left % digit != 0) {
+                    isSelfDividingNumber = false;
+                    break;
+                }
+            }
+            if (isSelfDividingNumber) {
+                result.add(left);
+            }
+            left++;
+        }
+        return result;
+    }
+}
