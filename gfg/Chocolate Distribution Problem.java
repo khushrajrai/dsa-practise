10690317eb564d41e04dc57b8456b22aa90ec3b0
Demo-Repos <khushrajrai@gmail.com>
2026-05-24 15:33:06 +0530

    Added Employees Earning More Than Their Managers SQL Question

--- gfg/Chocolate Distribution Problem.java
+++ gfg/Chocolate Distribution Problem.java
@@ -3,9 +3,7 @@
 // Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that -
 //       i. Each student gets exactly one packet.
 //      ii. The difference between maximum number of chocolates given to a student and minimum number is minimum and return that minimum possible difference.
-
 // Examples:
-
 // Input: arr = [3, 4, 1, 9, 56, 7, 9, 12], m = 5
 // Output: 6
 // Explanation: The minimum difference between maximum chocolates and minimum chocolates is 9 - 3 = 6 by choosing m packets as [3, 4, 9, 7, 9].
@@ -18,4 +16,18 @@
 // Constraints:
 // 1 ≤ m ≤ arr.size ≤ 105
 // 1 ≤ arr[i] ≤ 109
+class Solution {
 
+    public int findMinDiff(int arr[], int m) {
+        // code here
+        int n = arr.length;
+        int result = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+        for (int i = 0; i < n - m + 1; i++) {
+            int minEle = arr[i];
+            int maxEle = arr[i + m - 1];
+            result = Math.min(result, maxEle - minEle);
+        }
+        return result;
+    }
+}
