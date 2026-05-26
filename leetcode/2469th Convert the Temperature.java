120c36bdae0483dc185e38613e441c46d5f5fc08
Demo-Repos <khushrajrai@gmail.com>
2026-05-26 17:08:12 +0530

    Added Product Sales Analysis I

--- leetcode/2469th Convert the Temperature.java
+++ leetcode/2469th Convert the Temperature.java
@@ -28,3 +28,11 @@
 
 // 0 <= celsius <= 1000
 
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] result = new double[2];
+        result[0] = celsius+273.15;
+        result[1] = celsius*1.80+32.0;
+        return result;
+    }
+}
