7945742e6e4c00e32a8ac397f651e8638c8a406b
Demo-Repos <khushrajrai@gmail.com>
2026-05-26 19:27:30 +0530

    Added Count the Number of Special Characters I Leetcode Question

--- sql-leetcode/1068th Product Sales Analysis I.sql
+++ sql-leetcode/1068th Product Sales Analysis I.sql
@@ -11,7 +11,7 @@
 // | quantity    | int   |
 // | price       | int   |
 // +-------------+-------+
-// (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+// (sale_id, year) is primary key (combination of columns with unique values) of this table.
 // product_id is a foreign key (reference column) to Product table.
 // Each row of this table shows a sale on the product product_id in a certain year.
 // Note that the price is per unit.
