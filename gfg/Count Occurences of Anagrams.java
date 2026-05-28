dbf2f7c7a29f5376af994a7a75b0e819997e7df8
Demo-Repos <khushrajrai@gmail.com>
2026-05-28 23:25:07 +0530

    Added Count Occurences of Anagrams GFG Question

---
+++ gfg/Count Occurences of Anagrams.java
@@ -0,0 +1,18 @@
+// Count Occurences of Anagrams
+
+// Given a word pat and a text txt. Return the count of the occurrences of anagrams of the word in the text.
+
+// Example 1:
+
+// Input: txt = "forxxorfxdofr", pat = "for"
+// Output: 3
+// Explanation: for, orf and ofr appears in the txt, hence answer is 3.
+// Example 2:
+
+// Input: txt = "aabaabaa", pat = "aaba"
+// Output: 4
+// Explanation: aaba is present 4 times in txt.
+// Constraints:
+// 1 <= |pat| <= |txt| <= 105
+// Both strings contain lowercase English letters.
+
