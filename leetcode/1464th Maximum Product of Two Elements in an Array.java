f01351b23faca5d15223a336e00adee61138a50b
Demo-Repos <khushrajrai@gmail.com>
2026-05-30 19:33:09 +0530

    Added Count Good Triplets Leetcode Question

--- leetcode/1464th Maximum Product of Two Elements in an Array.java
+++ leetcode/1464th Maximum Product of Two Elements in an Array.java
@@ -24,3 +24,15 @@
 // 2 <= nums.length <= 500
 // 1 <= nums[i] <= 10^3
 
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProduct=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int product = (nums[i]-1)*(nums[j]-1);
+                maxProduct = Math.max(maxProduct,product);
+            }
+        }
+        return maxProduct;
+    }
+}
