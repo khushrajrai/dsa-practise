1d0d910e0f7f31f74046cdeabf5c45e10a8f8a47
Demo-Repos <khushrajrai@gmail.com>
2026-05-30 15:36:54 +0530

    Added Maximum Product of Two Elements in an Array Leetcode

--- leetcode/76th Minimum Window Substring.java
+++ leetcode/76th Minimum Window Substring.java
@@ -35,3 +35,49 @@
 // Follow up: Could you find an algorithm that runs in O(m + n) time?
 
  
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> freqMap = new HashMap<>();
+        //populating the map with t string
+        for(int i=0;i<t.length();i++){
+            char ch = t.charAt(i);
+            freqMap.put(ch,freqMap.getOrDefault(ch,0)+1);
+        }
+        int uniqueCharCount=freqMap.size();
+        int windowStart = 0;
+        int windowEnd = 0;
+        int startIndex = -1;
+        int minLen = Integer.MAX_VALUE;
+        int n = s.length();
+        while(windowEnd<n){
+            char ch = s.charAt(windowEnd);
+            //Expanding Phase
+            if(freqMap.containsKey(ch)){
+                freqMap.put(ch,freqMap.get(ch)-1);
+                if(freqMap.get(ch)==0){
+                    uniqueCharCount--;
+                }
+            }
+            while(uniqueCharCount==0){
+                //Shrinking Phase
+                int len = windowEnd-windowStart+1;
+                if(len<minLen){
+                    minLen = len;
+                    startIndex = windowStart;
+                }
+                ch = s.charAt(windowStart);
+                if(freqMap.containsKey(ch)){
+                    freqMap.put(ch,freqMap.get(ch)+1);
+                    if(freqMap.get(ch)>0){
+                        uniqueCharCount++;
+                    }
+                }
+                windowStart++;  
+            }
+            windowEnd++;
+        }
+        if(startIndex==-1) return "";
+        return s.substring(startIndex,startIndex+minLen);
+
+    }
+}
