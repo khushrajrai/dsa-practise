b4526ba24cdfe2f83e3a9d6e028543ed0e00dfa3
Demo-Repos <khushrajrai@gmail.com>
2026-06-02 00:37:23 +0530

    Added Max Consecutive Ones Leetcode Question

--- leetcode/167th Two Sum II - Input Array Is Sorted .java
+++ leetcode/167th Two Sum II - Input Array Is Sorted .java
@@ -16,16 +16,37 @@
 // Input: numbers = [-1,0], target = -1
 // Output: [1,2]
 // Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+//Two Pointer Approach
 class Solution {
 
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[]{i + 1, j + 1};
-                }
+        int n = numbers.length;
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (target == sum) {
+                return new int[]{l + 1, r + 1};
+            } else if (target < sum) {
+                r--;
+            } else {
+                l++;
             }
         }
         return new int[]{};
     }
 }
+
+//Brute Force
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         for (int i=0;i<numbers.length;i++){
+//             for(int j=i+1;j<numbers.length;j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     return new int[]{i+1,j+1};
+//                 }
+//             }
+//         }
+//         return new int[]{}  ;
+//     }
+// }
