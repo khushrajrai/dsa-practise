fbd9f3728605a833eefc414b8a433f0c11f91e30
Demo-Repos <khushrajrai@gmail.com>
2026-06-01 19:41:52 +0530

    Added Sliding Window Maximum Leetcode Qs

--- leetcode/239th Sliding Window Maximum.java
+++ leetcode/239th Sliding Window Maximum.java
@@ -1,13 +1,8 @@
 // 239. Sliding Window Maximum
 
 // You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-
 // Return the max sliding window.
-
- 
-
 // Example 1:
-
 // Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
 // Output: [3,3,5,5,6,7]
 // Explanation: 
@@ -20,14 +15,40 @@
 //  1  3  -1  -3 [5  3  6] 7       6
 //  1  3  -1  -3  5 [3  6  7]      7
 // Example 2:
-
 // Input: nums = [1], k = 1
 // Output: [1]
- 
-
 // Constraints:
-
 // 1 <= nums.length <= 105
 // -104 <= nums[i] <= 104
 // 1 <= k <= nums.length
+class Solution {
 
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int res[] = new int[n - k + 1];
+        if (n == 0) {
+            return res;
+        }
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        int index = 0;
+        while (index < k) {
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[index]) {
+                deque.pollLast();
+            }
+            deque.offerLast(index);
+            index++;
+        }
+        res[0] = nums[deque.peekFirst()];
+        for (int i = 1; i < n - k + 1; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() <= (i - 1)) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i + k - 1]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i + k - 1);
+            res[i] = nums[deque.peekFirst()];
+        }
+        return res;
+    }
+}
