5fc4f53e9c3d7b9c949cd42dce1ae9e213397820
Demo-Repos <khushrajrai@gmail.com>
2026-06-05 10:17:46 +0530

    Added Number of Arithmetic Triplets Leetcode Question

--- leetcode/415th Add Strings.java
+++ leetcode/415th Add Strings.java
@@ -25,3 +25,23 @@
 // 1 <= num1.length, num2.length <= 104
 // num1 and num2 consist of only digits.
 // num1 and num2 don't have any leading zeros except for the zero itself.
+
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(i>=0||j>=0||carry!=0){
+            int d1=(i>=0)?num1.charAt(i)-'0':0;
+            int d2=(j>=0)?num2.charAt(j)-'0':0;
+            int sum = d1+d2+carry;
+            carry = sum/10;
+            sb.append(sum%10);
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
