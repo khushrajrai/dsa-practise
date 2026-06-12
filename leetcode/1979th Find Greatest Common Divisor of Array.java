e98b7db27e8e950ffe6d9cd7ea13b5a3533b87ec
Demo-Repos <khushrajrai@gmail.com>
2026-06-12 09:51:24 +0530

    Added Find the Highest Altitude LC Question

--- leetcode/1979th Find Greatest Common Divisor of Array.java
+++ leetcode/1979th Find Greatest Common Divisor of Array.java
@@ -1,13 +1,8 @@
 // 1979. Find Greatest Common Divisor of Array
 
 // Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
-
 // The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
-
- 
-
 // Example 1:
-
 // Input: nums = [2,5,6,9,10]
 // Output: 2
 // Explanation:
@@ -15,7 +10,6 @@
 // The largest number in nums is 10.
 // The greatest common divisor of 2 and 10 is 2.
 // Example 2:
-
 // Input: nums = [7,5,6,8,3]
 // Output: 1
 // Explanation:
@@ -23,17 +17,26 @@
 // The largest number in nums is 8.
 // The greatest common divisor of 3 and 8 is 1.
 // Example 3:
-
 // Input: nums = [3,3]
 // Output: 3
 // Explanation:
 // The smallest number in nums is 3.
 // The largest number in nums is 3.
 // The greatest common divisor of 3 and 3 is 3.
- 
-
 // Constraints:
-
 // 2 <= nums.length <= 1000
 // 1 <= nums[i] <= 1000
+class Solution {
 
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int a = nums[nums.length - 1];
+        int b = nums[0];
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
