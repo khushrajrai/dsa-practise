0ff0571e16a18296414f9895e8426dd3d261ccdf
Demo-Repos <khushrajrai@gmail.com>
2026-06-22 17:18:10 +0530

    Added First Unique Character in a String Question LC

--- leetcode/1344th Angle Between Hands of a Clock.java
+++ leetcode/1344th Angle Between Hands of a Clock.java
@@ -28,3 +28,11 @@
 // 1 <= hour <= 12
 // 0 <= minutes <= 59
 
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double x = (hour%12)*30 + minutes/2.0;
+        double y = minutes * 6;
+        double angle = Math.abs(x-y);
+        return Math.min(angle,360-angle);
+    }
+}
