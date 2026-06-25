e66f3cc4ae5da50d8dd09c5632750ff81fd78da6
Demo-Repos <khushrajrai@gmail.com>
2026-06-25 17:50:56 +0530

    Added Leetcode Question Maximum Sum of Distinct Subarrays With Length K

--- leetcode/46th Permutations.java
+++ leetcode/46th Permutations.java
@@ -24,3 +24,64 @@
 // -10 <= nums[i] <= 10
 // All the integers of nums are unique.
 
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        backtrack(nums,0,res);
+        return res;
+    }
+    public void backtrack(int[] nums,int index,List<List<Integer>> res){
+        if(index==nums.length){
+            List<Integer> arr=new ArrayList<>();
+            for(int i:nums){
+                arr.add(i);
+            }
+            res.add(arr);
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            swap(nums,i,index);
+            backtrack(nums,index+1,res);
+            swap(nums,i,index);
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
+
+// Not Working For this 
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         int n = nums.length;
+//         int fact=1;
+//         for(int i=nums.length;i>=1;i--){
+//             fact=fact*i;
+//         }
+//         List<List<Integer>> res = new ArrayList<>();
+//         List<Integer> arr = new ArrayList<>();
+//         for(int a:arr){
+//             arr.add(a);
+//         }
+//         res.add(arr);
+//         int indexFirst = 0;
+//         int indexSecond = 1;
+//         for(int i=0;i<fact;i++){
+//             res.add(new ArrayList<>(arr));
+//             if(indexSecond<=n-1){
+//                 int temp=arr.get(indexFirst);
+//                 arr.set(indexFirst,arr.get(indexSecond));
+//                 arr.set(indexSecond,temp);
+//                 indexFirst++;
+//                 indexSecond++;
+//             }
+//             if(indexSecond==n){
+//                 indexFirst=0;
+//                 indexSecond=1;
+//             }
+//         }
+//         return res;
+//     }
+// }
