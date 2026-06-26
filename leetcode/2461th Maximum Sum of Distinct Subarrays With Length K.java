999a891d4c523f40117d73203bb27aa3497e84e0
Demo-Repos <khushrajrai@gmail.com>
2026-06-26 13:29:43 +0530

    Added Multiply Strings Leetcode Question

--- leetcode/2461th Maximum Sum of Distinct Subarrays With Length K.java
+++ leetcode/2461th Maximum Sum of Distinct Subarrays With Length K.java
@@ -1,17 +1,11 @@
 // 2461. Maximum Sum of Distinct Subarrays With Length K
 
 // You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
-
 // The length of the subarray is k, and
 // All the elements of the subarray are distinct.
 // Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
-
 // A subarray is a contiguous non-empty sequence of elements within an array.
-
- 
-
 // Example 1:
-
 // Input: nums = [1,5,4,2,9,9,9], k = 3
 // Output: 15
 // Explanation: The subarrays of nums with length 3 are:
@@ -22,18 +16,38 @@
 // - [9,9,9] which does not meet the requirements because the element 9 is repeated.
 // We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
 // Example 2:
-
 // Input: nums = [4,4,4], k = 3
 // Output: 0
 // Explanation: The subarrays of nums with length 3 are:
 // - [4,4,4] which does not meet the requirements because the element 4 is repeated.
 // We return 0 because no subarrays meet the conditions.
- 
-
 // Constraints:
-
 // 1 <= k <= nums.length <= 105
 // 1 <= nums[i] <= 105
- 
 // /
+class Solution {
 
+    public long maximumSubarraySum(int[] nums, int k) {
+        long sum = 0;
+        long maxSum = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+            if (i >= k) {
+                int prev = nums[i - k];
+                sum -= prev;
+                int count = freq.get(prev);
+                if (count == 1) {
+                    freq.remove(prev);
+                } else {
+                    freq.put(prev, count - 1);
+                }
+            }
+            if (i >= k - 1 && freq.size() == k) {
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
