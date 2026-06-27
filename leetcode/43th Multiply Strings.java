4b99c94a0f9c11829dfbd3c559ba693da5493f64
Demo-Repos <khushrajrai@gmail.com>
2026-06-27 12:24:03 +0530

    Added Clumsy Factorial Leetcode Problem

--- leetcode/43th Multiply Strings.java
+++ leetcode/43th Multiply Strings.java
@@ -22,3 +22,37 @@
 // num1 and num2 consist of digits only.
 // Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 
+class Solution {
+    public String multiply(String num1, String num2) {
+        if("0".equals(num1)||"0".equals(num2)){
+            return "0";
+        }
+        int ans[] = new int[num1.length()+num2.length()-1];
+        for(int i=0;i<num1.length();i++){
+            for(int j=0;j<num2.length();j++){
+                ans[i+j]+=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+            }
+        }
+        for(int i=ans.length-1;i>0;i--){
+            ans[i-1]+=ans[i]/10;
+            ans[i]%=10;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i : ans){
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+}
+
+
+// Not here Working as of Constraints
+// class Solution {
+//     public String multiply(String num1, String num2) {
+//         int str1 = Integer.parseInt(num1);
+//         int str2 = Integer.parseInt(num2);
+//         int res = str1*str2;
+//         String result = Integer.toString(res);
+//         return result;
+//     }
+// }
