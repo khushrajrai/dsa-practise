933783e1900bf5230db109a77290fffdbfe48d89
Demo-Repos <khushrajrai@gmail.com>
2026-06-29 10:03:19 +0530

    Added Excel Sheet Column Title Leetcode Question

--- leetcode/171th Excel Sheet Column Number.java
+++ leetcode/171th Excel Sheet Column Number.java
@@ -34,3 +34,12 @@
 // columnTitle consists only of uppercase English letters.
 // columnTitle is in the range ["A", "FXSHRXW"].
 
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        for(int i=0;i<columnTitle.length();i++){
+            result=result*26+(columnTitle.charAt(i)-'A'+1);
+        }
+        return result;
+    }
+}
