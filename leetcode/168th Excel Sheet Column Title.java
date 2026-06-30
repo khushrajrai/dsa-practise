b911eb88b28d1dc07c13cef84e2008c50fdf8c3a
Demo-Repos <khushrajrai@gmail.com>
2026-06-30 16:49:48 +0530

    Added Reverse Linked List Question

--- leetcode/168th Excel Sheet Column Title.java
+++ leetcode/168th Excel Sheet Column Title.java
@@ -32,3 +32,14 @@
 
 // 1 <= columnNumber <= 231 - 1
 
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while(columnNumber>0){
+            columnNumber--;
+            result.append((char)('A'+columnNumber%26));
+            columnNumber/=26;
+        }
+        return result.reverse().toString();
+    }
+}
