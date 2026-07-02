e009faa1b1d5e7daaa9c074e9866b5b318d0d401
Demo-Repos <khushrajrai@gmail.com>
2026-07-02 19:21:36 +0530

    Added Delete Node in a Linked List Leetcode Question

--- leetcode/1290th Convert Binary Number in a Linked List to Integer.java
+++ leetcode/1290th Convert Binary Number in a Linked List to Integer.java
@@ -26,3 +26,25 @@
 // Number of nodes will not exceed 30.
 // Each node's value is either 0 or 1.
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int sum=0;
+        while(head!=null){
+            sum*=2;
+            sum+=head.val;
+            head=head.next;
+        }
+        return sum;
+        
+    }
+}
