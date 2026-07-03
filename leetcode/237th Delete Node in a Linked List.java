7ab3663258670e390a96a0dc75503cac1ae967c9
Demo-Repos <khushrajrai@gmail.com>
2026-07-03 10:07:17 +0530

    Added Middle of the Linked List Question LC

--- leetcode/237th Delete Node in a Linked List.java
+++ leetcode/237th Delete Node in a Linked List.java
@@ -40,3 +40,18 @@
 // The value of each node in the list is unique.
 // The node to be deleted is in the list and is not a tail node.
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+        
+    }
+}
