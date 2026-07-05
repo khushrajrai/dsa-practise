a70bde5cef9b7164a8f2cff83ec774e41096582d
Demo-Repos <khushrajrai@gmail.com>
2026-07-05 08:01:41 +0530

    Added Merge Two Sorted Lists LC Question

--- leetcode/142th Linked List Cycle II.java
+++ leetcode/142th Linked List Cycle II.java
@@ -37,3 +37,36 @@
 
 // Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow){
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){
+            return null;
+        }
+        fast=head;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return fast;
+    }
+}
