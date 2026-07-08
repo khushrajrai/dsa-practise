4f5e89e42d43515151fb457efe0fa39bb7eda0a6
Demo-Repos <khushrajrai@gmail.com>
2026-07-08 18:47:14 +0530

    Added Linked List Cycle LC Question

--- leetcode/160th Intersection of Two Linked Lists.java
+++ leetcode/160th Intersection of Two Linked Lists.java
@@ -60,3 +60,25 @@
 
 // Follow up: Could you write a solution that runs in O(m + n) time and use only O(1) memory?
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ptrA = headA;
+        ListNode ptrB = headB;
+        while(ptrA!=ptrB){
+            ptrA = ptrA==null ? headB : ptrA.next;
+            ptrB = ptrB==null ? headA : ptrB.next;
+        }
+        return ptrA;
+    }
+}
