e425139b2e6b40475a97c57164a024858ede433d
Demo-Repos <khushrajrai@gmail.com>
2026-07-15 19:42:51 +0530

    Added GCD of Odd and Even Sums Leetcode Question

--- leetcode/2095th Delete the Middle Node of a Linked List.java
+++ leetcode/2095th Delete the Middle Node of a Linked List.java
@@ -39,3 +39,39 @@
 
 // The number of nodes in the list is in the range [1, 105].
 // 1 <= Node.val <= 105
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ //using delte node method for any position
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next==null){
+            return null;
+        }
+        int len=0;
+        ListNode temp=head;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        int pos=len/2;
+        ListNode prev=null;
+        temp=head;
+        for(int i=0;i<pos;i++){
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=temp.next;
+        temp.next=null;
+        return head;
+    }
+}
