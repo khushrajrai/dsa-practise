6670f7135d2254842423ce85d4cbda42112d6771
Demo-Repos <khushrajrai@gmail.com>
2026-07-18 12:52:13 +0530

    Added Merge k Sorted Lists Leetcode Question

--- leetcode/19th Remove Nth Node From End of List.java
+++ leetcode/19th Remove Nth Node From End of List.java
@@ -29,3 +29,41 @@
 
 // Follow up: Could you do this in one pass?
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null || head.next==null){
+            return null; 
+            // as in constraint we have that n at least 1 and sz at least 1 - so if size is 1 than returns null
+        }
+        ListNode temp=head;
+        int count=0;
+        while(temp!=null){
+            temp=temp.next;
+            count++;
+        }
+        int size=count-n;
+        if(size==0){
+            return head.next;
+            // if on the first index than just giving head.next
+        }
+        ListNode prev=null;
+        ListNode curr=head;
+        for(int i=0;i<size;i++){
+            prev=curr;
+            curr=curr.next;
+        }
+        prev.next=curr.next;
+        curr.next=null;
+        return head;
+    }
+}
