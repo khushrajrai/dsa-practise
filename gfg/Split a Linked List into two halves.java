dad54f6eda9db942533d26833c87eeeb3f34c1af
Demo-Repos <khushrajrai@gmail.com>
2026-07-26 11:39:20 +0530

    Added GFQ Question - Split a Linked List into two halves

---
+++ gfg/Split a Linked List into two halves.java
@@ -0,0 +1,18 @@
+// Split a Linked List into two halves
+
+// Given a Circular linked list. The task is split into two Circular Linked lists. If there are an odd number of nodes in the given circular linked list then out of the resulting two halved lists, the first list should have one node more than the second list.
+
+// Examples :
+
+// Input: LinkedList : 10->4->9
+// Output: 10->4 , 9
+
+// Explanation: After dividing linked list into 2 parts , the first part contains 10, 4 and second part contain only 9.
+// Input: LinkedList : 10->4->9->10
+// Output: 10->4 , 9->10
+
+// Explanation: After dividing linked list into 2 parts , the first part contains 10, 4 and second part contain 9, 10.
+// Constraints:
+// 2 <= number of nodes <= 105
+// 1 <= node->data <= 103
+
--- leetcode/147th Insertion Sort List.java
+++ leetcode/147th Insertion Sort List.java
@@ -29,3 +29,51 @@
 // The number of nodes in the list is in the range [1, 5000].
 // -5000 <= Node.val <= 5000
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode temp=head.next;
+        ListNode sortedTail=head;
+        while(temp!=null){
+            if(temp.val>=sortedTail.val){
+                sortedTail=temp;
+                temp=temp.next;
+                continue;
+            }
+            sortedTail.next=temp.next;
+            //temp.next=null;
+            ListNode prev=null;
+            ListNode t1=head;
+            while(t1!=sortedTail.next){
+                if(t1.val>=temp.val){
+                    break;
+                }
+                prev=t1;
+                t1=t1.next;
+            }
+            //insert at head
+            if(prev==null){
+                temp.next=head;
+                head=temp;
+            }else{
+                //insert at specific position
+                temp.next=prev.next;
+                prev.next=temp;
+            }
+            temp=sortedTail.next;
+        }
+        return head;
+    }
+}
