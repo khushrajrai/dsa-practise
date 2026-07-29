7bf454b3c6094f1d9588809bc1706b95d41d0843
Demo-Repos <khushrajrai@gmail.com>
2026-07-29 18:32:53 +0530

    Added Minimum Number of Swaps to Make the String Balanced Question LC

--- leetcode/921th Minimum Add to Make Parentheses Valid.java
+++ leetcode/921th Minimum Add to Make Parentheses Valid.java
@@ -27,3 +27,45 @@
 // 1 <= s.length <= 1000
 // s[i] is either '(' or ')'.
 
+//more optimal using logic of how works in stack but not taking stack (but it takes extra of O(N) space)
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                open++;
+            }else{
+                if(open<=0){
+                    close++;
+                }else{
+                    open--;
+                }
+            }
+        }
+        return open+close;
+    }
+}
+
+//using stack (but it takes extra of O(N) space)
+
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='('){
+//                 stack.push(ch);
+//             }else{
+//                 if(stack.isEmpty() || stack.peek()==')'){
+//                     stack.push(ch);
+//                 }else{
+//                     stack.pop();
+//                 }
+//             }
+//         }
+//         return stack.size();
+//     }
+// }
