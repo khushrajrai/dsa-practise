a8581518b80c137630d0ecfda0ba792402410ce2
Demo-Repos <khushrajrai@gmail.com>
2026-07-30 19:23:56 +0530

    Updated Asteroid Collision Ques

--- leetcode/735th Asteroid Collision.java
+++ leetcode/735th Asteroid Collision.java
@@ -1,38 +1,64 @@
 // 735. Asteroid Collision
 
 // We are given an array asteroids of integers representing asteroids in a row. The indices of the asteroid in the array represent their relative position in space.
-
 // For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
-
 // Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
-
- 
-
 // Example 1:
-
 // Input: asteroids = [5,10,-5]
 // Output: [5,10]
 // Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
 // Example 2:
-
 // Input: asteroids = [8,-8]
 // Output: []
 // Explanation: The 8 and -8 collide exploding each other.
 // Example 3:
-
 // Input: asteroids = [10,2,-5]
 // Output: [10]
 // Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
 // Example 4:
-
 // Input: asteroids = [3,5,-6,2,-1,4]​​​​​​​
 // Output: [-6,2,4]
 // Explanation: The asteroid -6 makes the asteroid 3 and 5 explode, and then continues going left. On the other side, the asteroid 2 destroys -1. Since 2 and 4 are both moving right, they never collide.
- 
-
 // Constraints:
-
 // 2 <= asteroids.length <= 104
 // -1000 <= asteroids[i] <= 1000
 // asteroids[i] != 0/
+class Solution {
 
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < asteroids.length; i++) {
+            //if stack is empty or a positive value arrives than perform blind insertion
+            if (stack.isEmpty() || asteroids[i] > 0) {
+                stack.push(asteroids[i]);
+            } else {
+                while (!stack.isEmpty()) {
+                    int top = stack.peek();
+                    if (top < 0) {
+                        stack.push(asteroids[i]);
+                        break;
+                    }
+                    int modVal = Math.abs(asteroids[i]);
+                    if (modVal == top) {
+                        stack.pop();
+                        break;
+                    } else if (modVal < top) {
+                        break;
+                    } else {
+                        stack.pop();
+                        if (stack.isEmpty()) {
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int len = stack.size();
+        int ansArray[] = new int[len];
+        for (int i = len - 1; i >= 0; i--) {
+            ansArray[i] = stack.pop();
+        }
+        return ansArray;
+    }
+}
