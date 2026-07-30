82bc02674c036888504979986a4d14c2edcc55ad
Demo-Repos <khushrajrai@gmail.com>
2026-07-30 19:22:00 +0530

    Added Asteroid Collision Leetcode Question

--- leetcode/1963th Minimum Number of Swaps to Make the String Balanced.java
+++ leetcode/1963th Minimum Number of Swaps to Make the String Balanced.java
@@ -42,3 +42,22 @@
 // s[i] is either '[' or ']'.
 // The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.
 
+class Solution {
+    public int minSwaps(String s) {
+        int open=0;
+        int close=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='['){
+                open++;
+            }else{
+                if(open<=0){
+                    close++;
+                }else{
+                    open--;
+                }
+            }
+        }
+        return (close+1)/2;
+    }
+}
