e3f7601455deb8b954e397a57a71b88b6b739287
Demo-Repos <khushrajrai@gmail.com>
2026-08-06 19:18:25 +0530

    Added Minimum Number Game Leetcode Ques

--- leetcode/94th Binary Tree Inorder Traversal.java
+++ leetcode/94th Binary Tree Inorder Traversal.java
@@ -43,3 +43,33 @@
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    void dfs(TreeNode root){
+        if(root==null){
+            return;
+        }
+        dfs(root.left);
+        ans.add(root.val);
+        dfs(root.right);
+    }
+}
