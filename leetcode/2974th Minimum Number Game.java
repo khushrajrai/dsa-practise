42b0d9d99fe0f875789509654a1ecb2e69eefeab
Demo-Repos <khushrajrai@gmail.com>
2026-08-07 19:50:48 +0530

    Added Find the Kth Largest Integer in the Array Question Leetcode

--- leetcode/2974th Minimum Number Game.java
+++ leetcode/2974th Minimum Number Game.java
@@ -28,3 +28,14 @@
 // 1 <= nums[i] <= 100
 // nums.length % 2 == 0
 
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int arr[] = new int[nums.length];
+        for(int i=0;i<nums.length;i+=2){
+            arr[i]=nums[i+1];
+            arr[i+1]=nums[i];
+        }
+        return arr;
+    }
+}
