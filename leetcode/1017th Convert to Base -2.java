b15efd0906c540e0b65444e9b8b8ab5241704f27
Demo-Repos <khushrajrai@gmail.com>
2026-08-13 20:00:16 +0530

    Added Ant on the Boundary Leetcode Problem

--- leetcode/1017th Convert to Base -2.java
+++ leetcode/1017th Convert to Base -2.java
@@ -27,3 +27,19 @@
 
 // 0 <= n <= 109
 
+class Solution {
+    public String baseNeg2(int n) {
+        if(n==0) return "0";
+        StringBuilder sb = new StringBuilder();
+        while(n!=0){
+            int remainder= n%-2;
+            n/=-2;
+            if(remainder<0){
+                remainder+=2;
+                n++;
+            }
+            sb.append(remainder);
+        }
+        return sb.reverse().toString();
+    }
+}
