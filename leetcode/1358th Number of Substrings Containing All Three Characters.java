b43e1eab5a93ceb05fd1673c68fc95d5c4ecd941
Demo-Repos <khushrajrai@gmail.com>
2026-08-16 14:32:28 +0530

    Added Fruit Into Baskets Leetcode Ques

--- leetcode/1358th Number of Substrings Containing All Three Characters.java
+++ leetcode/1358th Number of Substrings Containing All Three Characters.java
@@ -27,3 +27,20 @@
 // 3 <= s.length <= 5 x 104
 // s only consists of 'a', 'b' or 'c' characters.
 
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n=s.length();
+        int left=0;
+        int freq[] = new int[3];
+        int ans=0;
+        for(int right=0;right<n;right++){
+            freq[s.charAt(right)-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+                ans+=(n-right);
+                freq[s.charAt(left)-'a']--;
+                left++;
+            }
+        }
+        return ans;
+    }
+}
