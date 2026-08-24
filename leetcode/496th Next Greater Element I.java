df827479e5e786eab76471d11f93076d38cab7a8
Demo-Repos <khushrajrai@gmail.com>
2026-08-24 20:27:44 +0530

    Added Fizz Buzz Leetcode Ques

--- leetcode/496th Next Greater Element I.java
+++ leetcode/496th Next Greater Element I.java
@@ -37,3 +37,24 @@
 
 // Follow up: Could you find an O(nums1.length + nums2.length) solution?
 
+// Brute Force - Optimal Option - Stack + HashMap
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int res[] = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = -1;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    for (int k = j + 1; k < nums2.length; k++) {
+                        if (nums2[k] > nums1[i]) {
+                            res[i] = nums2[k];
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
