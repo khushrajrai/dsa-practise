fed14e27572c2605efe981926a4c55a8edca4499
Demo-Repos <khushrajrai@gmail.com>
2026-08-25 19:53:46 +0530

    Updated Top K Frequent Words Ques

--- leetcode/692th Top K Frequent Words.java
+++ leetcode/692th Top K Frequent Words.java
@@ -1,31 +1,36 @@
 // 692. Top K Frequent Words
 
 // iven an array of strings words and an integer k, return the k most frequent strings.
-
 // Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
-
- 
-
 // Example 1:
-
 // Input: words = ["i","love","leetcode","i","love","coding"], k = 2
 // Output: ["i","love"]
 // Explanation: "i" and "love" are the two most frequent words.
 // Note that "i" comes before "love" due to a lower alphabetical order.
 // Example 2:
-
 // Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
 // Output: ["the","is","sunny","day"]
 // Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
- 
-
 // Constraints:
-
 // 1 <= words.length <= 500
 // 1 <= words[i].length <= 10
 // words[i] consists of lowercase English letters.
 // k is in the range [1, The number of unique words[i]]
- 
-
 // Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+class Solution {
 
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> freq = new HashMap<>();
+        for (String word : words) {
+            freq.put(word, freq.getOrDefault(word, 0) + 1);
+        }
+        List<String> res = new ArrayList<>(freq.keySet());
+        Collections.sort(res, (a, b) -> {
+            if (freq.get(a) == freq.get(b)) {
+                return a.compareTo(b);
+            }
+            return freq.get(b) - freq.get(a);
+        });
+        return res.subList(0, k);
+    }
+}
