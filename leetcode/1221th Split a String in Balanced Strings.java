270679a29e6ee24d27b5ca158f08c0f002626e3a
Demo-Repos <khushrajrai@gmail.com>
2026-08-27 17:43:02 +0530

    Updated Split a String in Balanced Strings Ques

--- leetcode/1221th Split a String in Balanced Strings.java
+++ leetcode/1221th Split a String in Balanced Strings.java
@@ -33,3 +33,20 @@
 // s[i] is either 'L' or 'R'.
 // s is a balanced string.
 
+class Solution {
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int count = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (s.charAt(i)=='L') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance==0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
