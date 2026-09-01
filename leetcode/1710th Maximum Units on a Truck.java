e51a46bf53bba0ebba499dcdd76b0cdf19293fa9
Demo-Repos <khushrajrai@gmail.com>
2026-09-01 12:23:28 +0530

    Added Maximum Bags With Full Capacity of Rocks Leetcode Question

--- leetcode/1710th Maximum Units on a Truck.java
+++ leetcode/1710th Maximum Units on a Truck.java
@@ -32,3 +32,20 @@
 // 1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
 // 1 <= truckSize <= 106
 
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes,(a,b)->b[1]-a[1]);
+        int res=0;
+        for(int boxType[] : boxTypes){
+            if(boxType[0]>=truckSize){
+                res+=(truckSize*boxType[1]);
+                truckSize=0;
+            }else{
+                res+=(boxType[0]*boxType[1]);
+                truckSize-=boxType[0];
+            }
+            if(truckSize==0) break;
+        }
+        return res;
+    }
+}
