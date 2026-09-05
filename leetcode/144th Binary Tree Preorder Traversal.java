51cf98f616b7019c987624c1b608f2fae4c0b0aa
Demo-Repos <khushrajrai@gmail.com>
2026-09-05 20:43:28 +0530

    Added Time Needed to Buy Tickets Leetcode Ques

--- leetcode/144th Binary Tree Preorder Traversal.java
+++ leetcode/144th Binary Tree Preorder Traversal.java
@@ -46,3 +46,60 @@
 
 // Follow up: Recursive solution is trivial, could you do it iteratively?
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //Iterative Approach
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if(node.right!=null){
+                stack.push(node.right);
+            }
+            if(node.left!=null){
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+}
+
+//Recursive Approach
+
+// class Solution {
+//     public List<Integer> printTraversal(TreeNode root,List<Integer> list) {
+//         if(root==null){
+//             return list;
+//         }
+//         list.add(root.val);
+//         printTraversal(root.left,list);
+//         printTraversal(root.right,list);
+//         return list;
+//     }
+//     public List<Integer> preorderTraversal(TreeNode root){
+//         List<Integer> list = new ArrayList<>();
+//         list=printTraversal(root,list);
+//         return list;
+//     }
+// }
