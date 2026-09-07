207741757e13be3d3563e6a49caa0f37d108cc44
Demo-Repos <khushrajrai@gmail.com>
2026-09-07 21:47:30 +0530

    Added Find All Anagrams in a String Leetcode Question

--- leetcode/1700th Number of Students Unable to Eat Lunch.java
+++ leetcode/1700th Number of Students Unable to Eat Lunch.java
@@ -39,3 +39,28 @@
 // sandwiches[i] is 0 or 1.
 // students[i] is 0 or 1.
 
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int cs=0;
+        int ss=0;
+        for(int choice : students){
+            if(choice==0) cs++;
+            if(choice==1) ss++;
+        }
+        for(int sandwich: sandwiches){
+            if(sandwich==0 && cs==0){
+                return ss;
+            }
+            if(sandwich==1 && ss==0){
+                return cs;
+            }
+            if(sandwich==0){
+                cs--;
+            }
+            if(sandwich==1){
+                ss--;
+            }
+        }
+        return 0;
+    }
+}
