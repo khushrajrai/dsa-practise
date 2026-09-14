addc821665e263365ff5704adcb6e52fb509d25f
Demo-Repos <khushrajrai@gmail.com>
2026-09-14 10:07:44 +0530

    Added Find Median from Data Stream LC Question

--- leetcode/621th Task Scheduler.java
+++ leetcode/621th Task Scheduler.java
@@ -44,3 +44,48 @@
 // tasks[i] is an uppercase English letter.
 // 0 <= n <= 100
 
+class Task implements Comparable<Task>{
+    int frequency;
+    int executionTime;
+    Task(int f,int t){
+        frequency=f;
+        executionTime=t;
+    }
+    public int compareTo(Task that){
+        return that.frequency-this.frequency; //dcrs.
+    }
+}
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character,Integer> freqMap=new HashMap<>();
+        for(char ch:tasks){
+            freqMap.put(ch,freqMap.getOrDefault(ch,0)+1);
+        }
+        //insertion in maxHeap PQ
+        PriorityQueue<Task> pq = new PriorityQueue<>();
+        //insert tasks in pq
+        for(char ch : freqMap.keySet()){
+            int freq=freqMap.get(ch);
+            pq.offer(new Task(freq,0));
+        }
+        Queue<Task> queue = new LinkedList<>();
+        int time=0;
+        while(!queue.isEmpty() || !pq.isEmpty()){
+            time++;
+            //check if there is a task in pq & process it
+            if(!pq.isEmpty()){
+                Task task = pq.poll();
+                task.frequency--;
+                if(task.frequency>0){
+                    task.executionTime=time+n;
+                    queue.offer(task);
+                } 
+            }
+            //shift the active process to the PQ
+            if(!queue.isEmpty() && queue.peek().executionTime==time){
+                pq.offer(queue.poll());
+            }
+        }
+        return time;
+    }
+}
