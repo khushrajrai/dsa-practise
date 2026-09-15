0636363c7224c1bff57aec9f447c6a1b66cb9423
Demo-Repos <khushrajrai@gmail.com>
2026-09-15 22:41:47 +0530

    Added Binary Tree Postorder Traversal Leetcode Question

--- leetcode/145th Binary Tree Postorder Traversal.java
+++ leetcode/145th Binary Tree Postorder Traversal.java
@@ -1,48 +1,99 @@
 // 145. Binary Tree Postorder Traversal
 
 // Given the root of a binary tree, return the postorder traversal of its nodes' values.
-
- 
-
 // Example 1:
-
 // Input: root = [1,null,2,3]
-
 // Output: [3,2,1]
-
 // Explanation:
-
-
-
 // Example 2:
-
 // Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
-
 // Output: [4,6,7,5,2,9,8,3,1]
-
 // Explanation:
-
-
-
 // Example 3:
-
 // Input: root = []
-
 // Output: []
-
 // Example 4:
-
 // Input: root = [1]
-
 // Output: [1]
-
- 
-
 // Constraints:
-
 // The number of the nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
- 
-
 // Follow up: Recursive solution is trivial, could you do it iteratively?
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//Recursive Approach
+// class Solution {
+//     public List<Integer> printTraversal(TreeNode root,List<Integer> list) {
+//         if(root==null){
+//             return list;
+//         }
+//         printTraversal(root.left,list);
+//         printTraversal(root.right,list);
+//         list.add(root.val);
+//         return list;
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root){
+//         List<Integer> list = new ArrayList<>();
+//         list=printTraversal(root,list);
+//         return list;
+//     }
+// }
+//Iterative Approach
+class Solution {
 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            //move to left
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            //move to right
+            if (stack.peek().right != null) {
+                node = stack.peek().right;
+            } else {
+                TreeNode temp = stack.pop();
+                list.add(temp.val);
+                while (!stack.isEmpty() && temp == stack.peek().right) {
+                    temp = stack.pop();
+                    list.add(temp.val);
+                }
+            }
+        }
+        return list;
+    }
+}
