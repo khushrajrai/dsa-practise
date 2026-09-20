1287fd76e54a2884ca2e57673dca7f8f1dbfd30f
Demo-Repos <khushrajrai@gmail.com>
2026-09-20 18:18:14 +0530

    Revised Longest Palindrome LC Problem

--- leetcode/409th Longest Palindrome.java
+++ leetcode/409th Longest Palindrome.java
@@ -1,43 +1,34 @@
 // 409. Longest Palindrome
 
 // Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
-
 // Letters are case sensitive, for example, "Aa" is not considered a palindrome.
-
- 
-
 // Example 1:
-
 // Input: s = "abccccdd"
 // Output: 7
 // Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
 // Example 2:
-
 // Input: s = "a"
 // Output: 1
 // Explanation: The longest palindrome that can be built is "a", whose length is 1.
- 
-
 // Constraints:
-
 // 1 <= s.length <= 2000
 // s consists of lowercase and/or uppercase English letters only.
-
 class Solution {
+
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
-        int res=0;
-        for(char ch : s.toCharArray()){
-            if(set.contains(ch)){
-                res+=2;
+        int result = 0;
+        for (char ch : s.toCharArray()) {
+            if (set.contains(ch)) {
+                result += 2;
                 set.remove(ch);
-            }else{
+            } else {
                 set.add(ch);
             }
         }
-        if(set.size()>0){
-            res+=1;
+        if (set.size() > 0) {
+            result += 1;
         }
-        return res;
+        return result;
     }
 }
