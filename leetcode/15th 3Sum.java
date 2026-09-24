733d083942f5d9e0088a4468d15ad53081aba06e
Demo-Repos <khushrajrai@gmail.com>
2026-09-24 19:11:24 +0530

    Revised Three Sum Question

--- leetcode/15th 3Sum.java
+++ leetcode/15th 3Sum.java
@@ -34,7 +34,7 @@ class Solution {
                 continue;
             }
             int j = i + 1;
-            int k = nums.length - 1;
+            int k=nums.length - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
