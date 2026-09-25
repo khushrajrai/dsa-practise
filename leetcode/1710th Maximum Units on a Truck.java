e61aea61fc9c99e910c15e2ab7ca1fa41989edfb
Demo-Repos <khushrajrai@gmail.com>
2026-09-25 10:55:42 +0530

    Revised Maximum Units on a Truck LC Question

--- leetcode/1710th Maximum Units on a Truck.java
+++ leetcode/1710th Maximum Units on a Truck.java
@@ -26,7 +26,7 @@ class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
         int result = 0;
-        for (int boxType[] : boxTypes) {
+        for(int boxType[] : boxTypes) {
             if (boxType[0] >= truckSize) {
                 result += (truckSize * boxType[1]);
                 truckSize = 0;
